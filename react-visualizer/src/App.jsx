197af7f06b417250a5bf521198453f16477a6159
Gabe Clewis <gclewis16@gmail.com>
2025-05-11 16:34:56 -0400

    Added basic text area to right side of screen
    Added a small text area to the right panel that will act as a code editor within the webpage

--- react-visualizer/src/App.jsx
+++ react-visualizer/src/App.jsx
@@ -17,21 +17,41 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Data Structures Visualizer</h1>
+    <div className="app-container">
 
-      <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="Enter value"/>
-
-      <button onClick={handlePush}>Push</button>
-      <button onClick={handlePop}>Pop</button>
-
-      <div id="stackContainer">
-        {[...stack].reverse().map((item, index) => (
-          <div key={index} className="stack-item">
-            {item}
-          </div>
-        ))}
+      <div>
+        
       </div>
+
+      <div className="left-pane">
+        <h1>Algorithms Visualizer</h1>
+
+        <input type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Enter value"/>
+
+        <button onClick={handlePush}>Push</button>
+        <button onClick={handlePop}>Pop</button>
+
+        <div id="stackContainer">
+          {[...stack].reverse().map((item, index) => (
+            <div key={index} className="stack-item">
+              {item}
+            </div>
+          ))}
+        </div>
+      </div>
+
+          <div className="right-pane">
+            <h1>Code Editor</h1>
+
+            <textarea placeholder="Type code here..." 
+            style={{ width: '100%', height: '90%' }}
+
+            />
+          </div>
+
     </div>
   )
 }
