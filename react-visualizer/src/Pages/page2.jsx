b841ab35061e3d67fc061bfa23c3475cffc57904
Gabe Clewis <gclewis16@gmail.com>
2025-05-16 12:14:22 -0400

    Added bubble sorting of bars and animations to show bars visually sort
    Created a sorting function and helper sleep function to allow each iteration to be delayed to actually show the sort itself. For now the sort is just bubble sort

--- react-visualizer/src/Pages/page2.jsx
+++ react-visualizer/src/Pages/page2.jsx
@@ -5,15 +5,44 @@ import { BarsVisualizer } from '../Components/BarsVisualizer'
 import "./Page2.css"
 
 export function Page2() {
-  const n = 20
+  const n = 50
   const [array, setArray] = useState([])
 
-  useEffect(() => {
+  // Fills the array with values of random heights
+  const generateRandomArray = () => {
     const newArray = []
     for (let i = 0; i < n; i++) {
       newArray.push(Math.random() * 100)
     }
     setArray(newArray)
+  }
+
+  // Uses the bubble sort to sort the array of heights
+  const bubbleSort = async () => {
+    const sortedArray = [...array]
+
+    for (let i = 0; i < sortedArray.length - 1; i++) {
+      for (let j = 0; j < sortedArray.length - i - 1; j++) {
+        if (sortedArray[j] > sortedArray[j + 1]) {
+          let temp = sortedArray[j]
+          sortedArray[j] = sortedArray[j + 1]
+          sortedArray[j + 1] = temp
+
+          setArray([...sortedArray])
+
+          await sleep(1)
+        }
+      }
+    }
+  }
+
+  // Helper function to delay each sorting animation so the user can see the visual movement
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  useEffect(() => {
+    generateRandomArray()
   }, [])
 
 
@@ -24,6 +53,11 @@ export function Page2() {
       <div className="algorithmWindow">
         <h1>Visualizer 2</h1>
         <BarsVisualizer array={array}/>
+      </div>
+
+      <div className="buttonPanel">
+        <button onClick={generateRandomArray}>Generate</button>
+        <button onClick={bubbleSort}>Play</button>
       </div>
     </div>
   )
